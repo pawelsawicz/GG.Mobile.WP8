6be3f1af7a2678470066a998f4fa4c6aaa271417
pawelsawicz <pawel@sawicz.eu>
2014-05-15 14:49:17 +0200

    initialize project & configured Caliburn.Micro

---
+++ JustGiving.WP8/Bootstrapper.cs
@@ -0,0 +1,37 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JustGiving.WP8.ViewModels;
+namespace JustGiving.WP8
+{
+    public class Bootstrapper : PhoneBootstrapper
+    {
+        PhoneContainer _phoneContainer;
+
+        protected override void Configure()
+        {
+            _phoneContainer = new PhoneContainer();
+            _phoneContainer.RegisterPhoneServices(RootFrame);
+            _phoneContainer.PerRequest<MainPageViewModel>();
+            
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _phoneContainer.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _phoneContainer.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _phoneContainer.BuildUp(instance);
+        }
+    }
+}
---
+++ JustGiving.WP8/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using JustGiving.WP8.Resources;
+
+namespace JustGiving.WP8
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
