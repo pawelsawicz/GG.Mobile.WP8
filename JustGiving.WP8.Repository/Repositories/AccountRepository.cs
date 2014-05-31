bb3d0cd509db390f7a07d4d34559a521bbf75089
pawelsawicz <pawel@sawicz.eu>
2014-06-01 00:46:05 +0200

    createing accounts #2 first implementation - basic

--- JustGiving.WP8.Repository/Repositories/AccountRepository.cs
+++ JustGiving.WP8.Repository/Repositories/AccountRepository.cs
@@ -11,26 +11,31 @@ namespace JustGiving.WP8.Repository.Repositories
 {
     public class AccountRepository : RepositoryBase
     {
+        private readonly XmlDeserializer _deserializer;
+
+        public AccountRepository()
+        {
+            _deserializer = new XmlDeserializer();
+        }
+
         public async Task<bool> AuthenticateAccount(string userName, string password)
         {           
             Client.Authenticator = new HttpBasicAuthenticator(userName, password);
             Request.Resource = "/account";
             Request.Method = Method.GET;
             Request.RequestFormat = DataFormat.Xml;
-            var response = await Client.GetResponseAsync(Request);            
-            XmlDeserializer deserializer = new XmlDeserializer();
-            var model = deserializer.Deserialize<AccountVerefication>(response);
+            var response = await Client.GetResponseAsync(Request);
+            var responseModel = _deserializer.Deserialize<AccountVerefication>(response);
 
-            if (model != null)
+            if (responseModel != null)
             {
-                AddAccountToSession(model);
+                AddAccountToSession(responseModel);
                 return true;
             }
             else
             {
                 return false;
             }
-
         }
 
         private void AddAccountToSession(AccountVerefication model)
@@ -40,6 +45,25 @@ namespace JustGiving.WP8.Repository.Repositories
             if (!settings.Contains("userAccount"))
             {
                 settings.Add("userAccount", model);
+            }
+        }
+
+        public async Task<AccountRegistrationResponse> RegisterAccount(AccountRegistration model)
+        {
+            Request.Resource = "/account";
+            Request.Method = Method.PUT;
+            Request.RequestFormat = DataFormat.Json;
+            Request.AddBody(model);
+            var response = await Client.GetResponseAsync(Request);
+            var responseModel = _deserializer.Deserialize<AccountRegistrationResponse>(response);
+
+            if (responseModel != null)
+            {
+                return responseModel;
+            }
+            else
+            {
+                return null;
             }
         }
     }
@@ -55,5 +79,39 @@ namespace JustGiving.WP8.Repository.Repositories
         public decimal totalRaised { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
+    }
+
+    public class AccountRegistration
+    {
+        public AccountRegistration()
+        {
+            Address = new Address();
+        }
+
+        public bool AcceptTermsAndConditions { get; set; }
+        public Address Address { get; set; }
+        public int CauseId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Reference { get; set; }
+        public string Title { get; set; }
+    }
+
+    public class AccountRegistrationResponse
+    {
+        public string Country { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class Address
+    {
+        public string Country { get; set; }
+        public string CountyOrState { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string PostCodeOrZipCode { get; set; }
+        public string TownOrCity { get; set; }
     }
 }
--- JustGiving.WP8/ViewModels/Access/RegistrationViewModel.cs
+++ JustGiving.WP8/ViewModels/Access/RegistrationViewModel.cs
@@ -4,15 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using JustGiving.WP8.Repository.Repositories;
+using System.Windows;
+
 namespace JustGiving.WP8.ViewModels.Access
 {
     public class RegistrationViewModel : PropertyChangedBase 
     {
         private readonly INavigationService _navigationService;
+        private readonly AccountRepository _accountRepository;
+        public AccountRegistration AccountRegistrationModel { get; set; }
+
 
         public RegistrationViewModel(INavigationService navigationService)
         {
             _navigationService = navigationService;
+            _accountRepository = new AccountRepository();
+            AccountRegistrationModel = new AccountRegistration();
+            AccountRegistrationModel.AcceptTermsAndConditions = true;
+        }
+
+        public async void TrySignUp()
+        {
+            var result = await _accountRepository.RegisterAccount(AccountRegistrationModel);
+            if (result != null)
+            {
+                var resultMessage = string.Format("{0} has been created", result.Email);
+                MessageBox.Show(resultMessage);
+                _navigationService.UriFor<LoginViewModel>().Navigate();
+            }
+            else
+            {
+                MessageBox.Show("Something goes wrong please try again!");
+            }
         }
 
         public void NavigateToHelp()
