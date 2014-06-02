3c6c29ffea40d837966624931a0bd46a01ed9b94
pawelsawicz <pawel@sawicz.eu>
2014-06-02 16:56:27 +0200

    class that contains insolate storage objects

---
+++ JustGiving.WP8.Repository/UserHelper.cs
@@ -0,0 +1,59 @@
+﻿using JustGiving.WP8.Repository.Models;
+using JustGiving.WP8.Repository.Repositories;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustGiving.WP8.Repository
+{
+    public static class UserHelper
+    {
+        public static IsolatedStorageSettings _insolatedStorageSettings = IsolatedStorageSettings.ApplicationSettings;
+
+        public static Account BasicAuthenticator
+        {
+            get
+            {
+                return (Account)_insolatedStorageSettings["BasicAuthenticator"];
+            }
+            set
+            {
+                if (_insolatedStorageSettings.Contains("BasicAuthenticator"))
+                {
+                    _insolatedStorageSettings["BasicAuthenticator"] = value;
+                }
+                else
+                {
+                    _insolatedStorageSettings.Add("BasicAuthenticator", value);
+                }
+
+                _insolatedStorageSettings.Save();
+            }
+        }
+
+        public static AccountVerefication User
+        {
+            get
+            {
+                return (AccountVerefication)_insolatedStorageSettings["userAccount"];
+            }
+            set
+            {
+                if (_insolatedStorageSettings.Contains("userAccount"))
+                {
+                    _insolatedStorageSettings["userAccount"] = value;
+                }
+                else
+                {
+                    _insolatedStorageSettings.Add("userAccount", value);
+                }
+
+                _insolatedStorageSettings.Save();
+            }
+        }
+    }
+}
